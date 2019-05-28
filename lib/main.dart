bdcb4b79a9664c8974d4ff68c5fa96fb91013262
runcycle <justinbartholomew@gmail.com>
2019-05-28 15:56:36 -0400

    Added logic to create random dice faces when pressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,23 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+ int leftDiceNumber = 1;
+ int rightDiceNumber = 1;
+
+ void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+  });
+ }
+
   @override
   Widget build(BuildContext context) {
     return 
@@ -27,17 +44,17 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Left button got pressed.');
+                changeDiceFace();
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
               onPressed: (){
-                print('Right button got pressed.');
+                changeDiceFace();
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
@@ -45,3 +62,4 @@ class DicePage extends StatelessWidget {
     );
   }
 }
+
